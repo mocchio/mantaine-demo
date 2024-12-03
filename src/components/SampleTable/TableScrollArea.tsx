f27927475dd5936634ede89bae297af82edd9344
Takumi Sakamoto <124575646+mocchio@users.noreply.github.com>
2024-12-03 13:12:11 +0900

    feat: Tabler iconsとモーダルの追加

--- src/components/SampleTable/TableScrollArea.tsx
+++ src/components/SampleTable/TableScrollArea.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
+import { IconPencil, IconTrash } from '@tabler/icons-react';
 import cx from 'clsx';
-import { ScrollArea, Table } from '@mantine/core';
+import { ActionIcon, Group, Modal, ScrollArea, Table, Text } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
 import classes from './TableScrollArea.module.css';
 
 const data = [
@@ -158,27 +160,50 @@ const data = [
 
 export function TableScrollArea() {
   const [scrolled, setScrolled] = useState(false);
+  const [opened, { open, close }] = useDisclosure(false);
 
   const rows = data.map((row) => (
     <Table.Tr key={row.name}>
       <Table.Td>{row.name}</Table.Td>
       <Table.Td>{row.email}</Table.Td>
       <Table.Td>{row.company}</Table.Td>
+      <Table.Td>
+        <Group gap={0} justify="flex-end">
+          <ActionIcon variant="subtle" color="gray" onClick={open}>
+            <IconPencil />
+          </ActionIcon>
+          <ActionIcon variant="subtle" color="red">
+            <IconTrash />
+          </ActionIcon>
+        </Group>
+      </Table.Td>
     </Table.Tr>
   ));
 
+  const modalBox = (
+    <Group>
+      <Text>edit data</Text>
+    </Group>
+  );
+
   return (
-    <ScrollArea h={300} onScrollPositionChange={({ y }) => setScrolled(y !== 0)}>
-      <Table miw={700}>
-        <Table.Thead className={cx(classes.header, { [classes.scrolled]: scrolled })}>
-          <Table.Tr>
-            <Table.Th>Name</Table.Th>
-            <Table.Th>Email</Table.Th>
-            <Table.Th>Company</Table.Th>
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>{rows}</Table.Tbody>
-      </Table>
-    </ScrollArea>
+    <>
+      <ScrollArea h={300} onScrollPositionChange={({ y }) => setScrolled(y !== 0)}>
+        <Table miw={700}>
+          <Table.Thead className={cx(classes.header, { [classes.scrolled]: scrolled })}>
+            <Table.Tr>
+              <Table.Th>Name</Table.Th>
+              <Table.Th>Email</Table.Th>
+              <Table.Th>Company</Table.Th>
+              <Table.Th />
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>{rows}</Table.Tbody>
+        </Table>
+      </ScrollArea>
+      <Modal opened={opened} onClose={close} title="Edit">
+        {modalBox}
+      </Modal>
+    </>
   );
 }
